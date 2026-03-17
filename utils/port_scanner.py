c95b4c09db09b340ef1db0ad1adadb52ab77b866
Logan Agunat <lagunat@zagmail.gonzaga.edu>
2026-03-17 10:18:49 -0700

    Working on port scanner

---
+++ utils/port_scanner.py
@@ -0,0 +1,45 @@
+############################################################
+#Author: Logan Agunat
+#Date created: 3/13/26
+#Date last modified:
+#Description: Contains helper functions for port scanner
+############################################################
+import socket
+
+def scan_port(ip_address: str, port: int) -> bool:
+    try:
+        # Create a TCP socket
+        #sock ← CREATE socket
+       with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+
+        # Set timeout so it doesn't hang
+        #SET sock timeout TO 1 second
+        sock.settimeout(timeout=1)
+        print(f"Connecting to {ip_address} and {port}...")
+
+        # Try to connect to the IP and port
+        #result ← CONNECT sock TO (ip_address, port)
+        result = sock.connect(ip_address, port)
+        print("Connected Succesfully!")
+      
+        if result == 0:
+           return True
+        else:
+           return False
+    except Exception:
+        return False
+    
+def scan_ports(ip_address: str, port_range: list) -> list:
+   #open_ports ← EMPTY LIST
+
+    #FOR each port IN port_range
+        #result ← scan_port(ip_address, port)
+
+        #IF result IS True THEN
+            #ADD port TO open_ports
+        #END IF
+   # END FOR
+
+    #RETURN open_ports
+
